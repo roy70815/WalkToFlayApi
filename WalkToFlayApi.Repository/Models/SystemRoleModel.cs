87234a12ef4aa10329d7d61ec2b74979e071a305
冠榮 邱 <roy70815@gmail.com>
2021-08-25 00:04:52 +0800

    新增基本Model類別

---
+++ WalkToFlayApi.Repository/Models/SystemRoleModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalkToFlayApi.Repository.Models
+{
+    /// <summary>
+    /// 規則(帳號綁角色)
+    /// </summary>
+    public class SystemRoleModel
+    {
+        /// <summary>
+        /// 規則Id
+        /// </summary>
+        public int RoleId { get; set; }
+
+        /// <summary>
+        /// 會員帳號 RF: Member.MemberId
+        /// </summary>
+        public string MemberId { get; set; }
+
+        /// <summary>
+        /// 角色IdRF: SystemRoleUser.RoleUserId
+        /// </summary>
+        public int RoleUserId { get; set; }
+    }
+}
