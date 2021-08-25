83f428a74096efc425f0498785f58f79d588f4e1
s18963224 <s18963224@gmail.com>
2021-08-25 20:55:59 +0800

    route 設定

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo:'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
