14ed81831263cc6535bb8588c73d451b7dce1f34
冠榮 邱 <roy70815@gmail.com>
2021-08-30 20:16:26 +0800

    修改登入API RestFul

---
+++ WalkToFlayApi/Infrastructure/Validation/LogginParameterValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalkToFlayApi.Models.Input;
+
+namespace WalkToFlayApi.Infrastructure.Validation
+{
+    /// <summary>
+    /// 登入參數驗證
+    /// </summary>
+    public class LogginParameterValidator : AbstractValidator<LogginParameter>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogginParameterValidator"/> class.
+        /// </summary>
+        public LogginParameterValidator()
+        {
+            RuleFor(x => x.MemberId)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("帳號不能為空");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("帳號不能為空");
+        }
+    }
+}
