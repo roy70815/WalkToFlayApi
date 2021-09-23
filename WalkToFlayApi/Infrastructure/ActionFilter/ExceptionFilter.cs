255c9d5aee3b4d352adb39997ac48c29af688a52
冠榮 邱 <roy70815@gmail.com>
2021-09-23 21:39:11 +0800

    統一Exception輸出格式

---
+++ WalkToFlayApi/Infrastructure/ActionFilter/ExceptionFilter.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WalkToFlayApi.Models.Output;
+
+namespace WalkToFlayApi.Infrastructure.ActionFilter
+{
+    /// <summary>
+    /// Exception過濾
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Filters.IAsyncExceptionFilter" />
+    public class ExceptionFilter : IAsyncExceptionFilter
+    {
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            ApiVersionAttribute apiVersion = (ApiVersionAttribute)null;
+            var actionDescriptor = context.ActionDescriptor;
+            var controllerActionDescriptor = actionDescriptor as ControllerActionDescriptor;
+            if (controllerActionDescriptor != null)
+            {
+                apiVersion = (ApiVersionAttribute)controllerActionDescriptor.MethodInfo.GetCustomAttributes(typeof(ApiVersionAttribute), false).FirstOrDefault();
+                apiVersion = apiVersion ?? (ApiVersionAttribute)controllerActionDescriptor.ControllerTypeInfo.GetCustomAttributes(typeof(ApiVersionAttribute), true).FirstOrDefault();
+            }
+            var response = new FailOutputModel
+            {
+                Id = Guid.NewGuid(),
+                Method = $"{context.HttpContext.Request.Path}.{context.HttpContext.Request.Method}",
+                ApiVersion = apiVersion is null ? "1.0" : apiVersion.Versions.FirstOrDefault().ToString(),
+                Error = new FailInformation
+                {
+                    Domain = "WalkToFly",
+                    Message = context.Exception.Message,
+                    Description = context.Exception.ToString()
+                }
+            };
+
+            context.Result = new ObjectResult(response)
+            {
+                // 500
+                StatusCode = (int)HttpStatusCode.InternalServerError
+            };
+
+            // Exceptinon Filter只在ExceptionHandled=false時觸發
+            // 所以處理完Exception要標記true表示已處理
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ WalkToFlayApi/Models/Output/FailInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkToFlayApi.Models.Output
+{
+    /// <summary>
+    /// 錯誤訊息
+    /// </summary>
+    public class FailInformation
+    {
+        public string Domain { get; set; }
+        public string Message { get; set; }
+        public string Description { get; set; }
+    }
+}
